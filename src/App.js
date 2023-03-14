333cfbbdde1b3adb29cdd8d3344782f19b6c9367
gasangw <thomasgasangwa@gmail.com>
2023-03-14 04:20:38 +0200

    Implement the about page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './components/Home/Home';
+import About from './components/about/About';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <About />
     </div>
   );
 }
